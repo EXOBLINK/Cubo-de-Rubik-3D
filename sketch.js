63bcf2bffb7d64c93bc72ff053e3a317424429a6
Martha Berenice Millán Arámburo <Mberenicema@gmail.com>
2026-05-21 05:58:56 +0000

    limpiando el index y agregando presentacion

--- sketch.js
+++ sketch.js
@@ -3,13 +3,10 @@ let manos;
 let rawHandsData = []; // Aquí guarda lo que detecta Google
 let tamañoCubito = 90; 
 let Separacion = 1.0;  
+
+// Variables para controlar la rotación con una mano
 let rotacionX = 0;
 let rotacionY = 0;
-let Fuente;
-
-function preload(){
-  Fuente = loadFont('https://cdnjs.cloudflare.com/ajax/libs/ink/3.1.10/fonts/Roboto/Roboto-Regular.ttf');
-}
 
 function setup() {
   createCanvas(800, 600, WEBGL);
@@ -31,12 +28,10 @@ function setup() {
     minTrackingConfidence: 0.5
   });
 
-  // Cada vez que Google detecta manos, nos pasa los resultados
   manos.onResults((results) => {
     rawHandsData = results.multiHandLandmarks; 
   });
 
-  // Uso de la cámara web con   Google MediaPipe
   const camera = new Camera(video.elt, {
     onFrame: async () => {
       await manos.send({ image: video.elt });
@@ -47,63 +42,37 @@ function setup() {
   camera.start();
 }
 
-
-
-
 function draw() {
   background(15, 15, 20);
-// --- 1. INSTRUCCIONES FIJAS EN LA ESQUINA (2D sobre 3D) ---
-  push();
-  translate(-width / 2, -height / 2); 
-  
-  // Solucionamos que el texto se esconda dándole una pequeñísima traslación al frente en el eje Z
-  translate(0, 0, 1); 
 
-  fill(0, 0, 0, 220); 
-  noStroke();
-  rect(15, 15, 255, 90, 8); 
-  
-  // Configuramos el texto con la fuente cargada
-  textFont(Fuente); // <--- REVISA QUE TENGA ESTA LÍNEA
-  fill(255);
-  textSize(14);
-  textAlign(LEFT, TOP);
-  text("🎮 CONTROL DE LAS MANOS:", 25, 25);
-  
-  textSize(12);
-  fill(230);
-  text("• Usa 1 Mano (Índice) para Girar el cubo", 25, 50);
-  text("• Usa 2 Manos para Separar / Unir cubitos", 25, 70);
-  pop(); 
-  // --- FIN DE INSTRUCCIONES FIJAS --- 
-
+  // Control con el mouse
   orbitControl();
   
-  //ILUMINACIÓN
+  // ILUMINACIÓN 
   ambientLight(120, 120, 120);
   pointLight(255, 255, 255, 350, -400, 500);   
   pointLight(160, 160, 160, -400, 400, -300);
 
-  // Ejecutamos el análisis de las señas de las manos
   analizarManos();
 
-  // ROTACIONES
+  // APLICAMOS ROTACIONES
   rotateX(rotacionX);
   rotateY(rotacionY);
 
-  //DIBUJANMOS LOS 27 CUBITOS ---
+  //DIBUJAMOS LOS 27 CUBITOS
   for (let x = -1; x <= 1; x++) {
     for (let y = -1; y <= 1; y++) {
       for (let z = -1; z <= 1; z++) {
-      push();
+        push();
       
-        // POSICIONES
         let posX = x * tamañoCubito * Separacion;
-        let posY = y* tamañoCubito * Separacion;
-        let posZ = z * tamañoCubito *Separacion;
+        let posY = y * tamañoCubito * Separacion;
+        let posZ = z * tamañoCubito * Separacion;
         
         translate(posX, posY, posZ);
-        Material(x, y, z);
+        
+        Material(x, y, z); 
+        
         stroke(10, 10, 14);
         strokeWeight(1.5);
         box(tamañoCubito - 4); 
@@ -116,41 +85,39 @@ function draw() {
 
 // DETECTOR DE MANOS E INTERACTIVIDAD CON EL CUBO
 function analizarManos() {
-  
-  // DETECTA DOS MANOS PUES SEPARAR el cubo
+  // DETECTA DOS MANOS: SEPARAR el cubo
   if (rawHandsData && rawHandsData.length === 2) {
     let mano1 = rawHandsData[0][0]; 
     let mano2 = rawHandsData[1][0]; 
     let d = dist(mano1.x, mano1.y, mano2.x, mano2.y);
     
-    // Mapeamos los valores de la distancia de la pantalla al factor de separación
     Separacion = map(d, 0.2, 0.7, 1.0, 3.5, true);
   } 
-  // DETECTA UNA SOLA MANO, ROTAR el cubo 
+  // DETECTA UNA SOLA MANO: ROTAR el cubo 
   else if (rawHandsData && rawHandsData.length === 1) {
-     Separacion = lerp(Separacion, 1.0, 0.1);
+    Separacion = lerp(Separacion, 1.0, 0.1);
     
-    let indice = rawHandsData[0][8];
+    let indice = rawHandsData[0][8]; 
   
     rotacionY = map(indice.x, 0, 1, -TWO_PI, TWO_PI);
     rotacionX = map(indice.y, 0, 1, TWO_PI, -TWO_PI);
   } 
-  // NO DETECTA MANOS Rota automáticamente
-    else {
+  // NO DETECTA MANOS: Rota automáticamente
+  else {
     Separacion = lerp(Separacion, 1.0, 0.1);
     rotacionX += 0.005;
     rotacionY += 0.005;
   }
 }
-
 function Material(x, y, z) {
-  if      (x ===  1) ambientMaterial(255, 20,  20);  
-  else if (x === -1) ambientMaterial(255, 120, 0);  
-  else if (y ===  1) ambientMaterial(10,  60,  255); 
-  else if (y === -1) ambientMaterial(0,   210, 40); 
-  else if (z ===  1) ambientMaterial(245, 245, 245);
-   else               ambientMaterial(255, 230, 0);  
-}
+  shininess(100); 
+
+  if      (x ===  1) specularMaterial(255, 10,  10);  
+  else if (x === -1) specularMaterial(255, 110, 0);   
+  else if (y ===  1) specularMaterial(10,  70,  255); 
+  else if (y === -1) specularMaterial(0,   220, 30);  
+  else if (z ===  1) specularMaterial(245, 245, 250); 
+  else               specularMaterial(255, 215, 0);   }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
